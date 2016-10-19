a9eb074950225ddc2e9d3db6e971cb5dd72f9922
HurricanGod <1807363937@qq.com>
2016-10-20 00:49:49 +0800

    2016-10-19初次开启项目

---
+++ ThesisSelectSystem/ThesisSelectSystem/Scripts/MyAdd/CreateClass.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function ()
+{
+    $('#sub').click(function (event)
+    {
+        event.preventDefault();
+        alert("测试是否会提交数据");
+        var url = $(this).attr('href');
+        $('#sub').load(url);
+    });
+});
